af3702e41b6daa074a22186514daf5d1a6b88e16
Manojdeepan <2212011@nec,edu,in>
2025-04-10 23:57:16 +0530

    check

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ import { ToastContainer } from 'react-toastify'
 import Player from './pages/student/Player'
 import MyEnrollments from './pages/student/MyEnrollments'
 import Loading from './components/student/Loading'
-
+//manoj
 const App = () => {
 
   const isEducatorRoute = useMatch('/educator/*');
