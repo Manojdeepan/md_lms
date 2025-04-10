8825728a8ac6f67fb315f91994704533c2602ebd
Manojdeepan <2212011@nec,edu,in>
2025-04-11 00:23:49 +0530

    add cors

--- server/server.js
+++ server/server.js
@@ -17,7 +17,10 @@ await connectDB()
 await connectCloudinary()
 
 // Middlewares
-app.use(cors())
+app.use(cors({
+  origin: ['https://lms-edu-red.vercel.app'],
+  credentials: true
+}))
 app.use(clerkMiddleware())
 
 // Routes
